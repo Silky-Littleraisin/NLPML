71eb79488912504f8300d5b9dc5e7606fad75806
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-22 20:40:54 +0900

    0222

--- ch01/show_spiral_dataset.py
+++ ch01/show_spiral_dataset.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('..')
-from dataset import spiral 
+import spiral 
 import matplotlib .pyplot as plt 
 
 x,t = spiral.load_data()
 print('x',x.shape) #(300,2)
-print ('t',t.shape) #(300,3)
+print ('t',t.shaps) #(300,3)
