22c40847fb9018dd12eb768d9aa95751af8bb73a
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-25 21:34:59 +0900

    0225

--- test_000.py
+++ test_000.py
@@ -12,3 +12,24 @@ def create_contexts_target(corpus, window_size=1):
         contexts.append(cs)
 
     return np.array(contexts), np.array(target)
+
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .') # split comma as a word
+    words = text.split(' ')
+
+    word_to_id = {}
+    id_to_word = {}
+
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
+
+
+text = 'you say goodbye and I say hello.'
